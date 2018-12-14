315851d7fcd4ed07f5b5b50af10ef10b21944fcd
DK\ELB <elb@sitecore.net>
2018-12-14 13:38:11 +0200

    no message

---
+++ src/Sitecore.Support.299577/GetFromRouteUrl.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.IO;
+using Sitecore.Mvc.Configuration;
+using Sitecore.Mvc.Data;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Pipelines.Response.GetPageItem;
+using Sitecore.Sites;
+using System;
+using System.Linq;
+using System.Web.Routing;
+
+
+namespace Sitecore.Support.Mvc.Pipelines.Response.GetPageItem
+{
+    public class GetFromRouteUrl : Sitecore.Mvc.Pipelines.Response.GetPageItem.GetFromRouteUrl
+    {
+        protected override Item ResolveItem(GetPageItemArgs args)
+        {
+            string path = this.GetPath(args.RouteData);
+            Item item = Context.Item;
+            if (string.IsNullOrEmpty(path))
+            {
+                return null;
+            }
+            else
+            {
+                SiteContext site = Context.Site;
+                if ((site != null) && path.Equals(site.VirtualFolder.Substring(0, site.VirtualFolder.Length - 1)))
+                {
+                    return null;
+                }
+            }
+            return base.GetItem(path, args);
+        }
+    }
+}
